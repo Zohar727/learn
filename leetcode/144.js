69b8c845be0007b9be80e61a51fcff91899ea8ea
Zohar W <kingdee@HSZC1707-0059.kingdee.gbl>
2021-04-20 20:55:57 +0800

    none

---
+++ leetcode/144.js
@@ -0,0 +1,27 @@
+/**
+ * leetcode 144. 二叉树的前序遍历
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+function preOrderTree (root, res) {
+    if (root == null) {
+        return;
+    }
+    res.push(root.val);
+    preOrderTree(root.left, res);
+    preOrderTree(root.right, res);
+}
+ var preorderTraversal = function(root) {
+    let res = [];
+    // test
+    preOrderTree(root, res);
+    return res;
+};
