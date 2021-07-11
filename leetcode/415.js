9fa4e04c9c5d79a886f5b5da19eaa8b1174f9528
zohar <wuzhihao1996727@qq.com>
2021-07-11 10:29:14 +0800

    11

---
+++ leetcode/415.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+ var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let add = 0;
+    let ans = [];
+    while (i>=0 || j>=0 || add != 0) {
+        const x = i >= 0 ? num1.charAt(i) - '0' : 0;
+        const y = j >= 0 ? num2.charAt(j) - '0' : 0;
+        const res = x + y + add;
+        ans.push(res%10);
+        add = Math.floor(res/10);
+        i--;
+        j--;
+    }
+
+    return ans.reverse().join('');
+};
+
+console.log(addStrings('11', '123'));
